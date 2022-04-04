60124dc8ec97ab765018dae2ceb40f497185f1cd
billyyan <By869422>
2022-04-03 22:07:04 -0700

    Added ConstraintLayout

--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -2,11 +2,19 @@ package com.udacity.asteroidradar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import com.udacity.asteroidradar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var NavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
     }
 }
